73e78bab99c9509428d5b6853681df7b40bbb2e7
Wilmer Cristancho <wilmerc@imaginacolombia.com>
2022-08-17 08:22:48 -0500

    add module

--- _config/adminSidebar.js
+++ _config/adminSidebar.js
@@ -1,3 +1,9 @@
 const pages = config('pages') // Get Pages from config
 export default [
+    {
+        title: 'ireports.cms.sidebar.reportsInfo',
+        icon: 'fas fa-file-alt',
+        children: [
+        ]
+      },
 ]
