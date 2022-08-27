591aaa53f66d7337a2c9fce2018fdb5b80a30158
Adriana Cardoso <acardosof27@gmail.com>
2022-08-26 20:27:59 -0400

    update style chance + add sort

--- _components/reports/sections/feature.vue
+++ _components/reports/sections/feature.vue
@@ -1,6 +1,33 @@
 <template>
-    <div>
-        feature
+    <div class="tw-overflow-x-hidden">
+        <div class="q-stepper-title">
+            <h3 class="text-primary">Select columns and filters</h3>
+            <div></div>
+        </div>
+        <h4 class="text-primary tw-text-base tw-font-extrabold tw-mb-6">Select and sort the columns</h4>
+
+        <div  class="
+        tw-grid 
+        tw-gap-6 
+        tw-grid-cols-1 
+        md:tw-grid-cols-2 
+        xl:tw-grid-cols-3 
+        tw-my-4 tw-overflow-hidden
+        ">
+             <div v-for="(field, keyField) in formFields.reportsColumns" :key="keyField">
+                <dynamic-field class="check-report-1" :field="field"/>
+            </div>
+        </div>
+
+
+        <h4 class="text-primary tw-text-base tw-font-extrabold tw-mt-8 tw-mb-6">Select the first 4 filters</h4>
+        
+        <div class="tw-flex tw-space-x-4">
+            <div v-for="(field, keyField) in formFields.reportsFilters" :key="keyField">
+                <dynamic-field class="check-report-2" :field="field"/>
+            </div>
+        </div>
+
     </div>
 </template>
 
@@ -10,10 +37,120 @@ export default {
         return {
 
         };
-    }
+    },
+    computed: {
+        formFields() {
+            return {
+                reportsColumns: {
+                    iD: {
+                        type: 'checkbox',
+                        value: false,
+                        props: {
+                            label: 'ID',
+                            value: false,
+                        }
+                    },
+                    referenceId: {
+                        type: 'checkbox',
+                        props: {
+                            label: 'Reference ID',
+                        }
+                    },
+                    customer: {
+                        type: 'checkbox',
+                        props: {
+                            label: 'Customer',
+                        }
+                    },
+                    adHoc: {
+                        type: 'checkbox',
+                        props: {
+                            label: 'Ad Hoc',
+                        }
+                    }
+                },
+                reportsFilters: {
+                    customer: {
+                        type: 'checkbox',
+                        props: {
+                            label: 'Customer',
+                        }
+                    },
+                    status: {
+                        type: 'checkbox',
+                        props: {
+                            label: 'Status',
+                        }
+                    },
+                    date: {
+                        type: 'checkbox',
+                        props: {
+                            label: 'Date',
+                        }
+                    }
+                },
+            };
+        },
+    },
 }
 </script>
 
 <style>
+.check-report-1 {
+    @apply tw-relative tw-border tw-rounded-xl;
+    border-color: rgba(0,0,0,0.24);
+    padding-right: 30px;
+}
+.check-report-1 .q-item__label--caption {
+    @apply tw-font-medium  tw-text-black tw-text-sm;
+ }
+.check-report-1:before {
+    @apply tw-right-2.5 tw-w-4 tw-h-4 tw-block tw-bg-no-repeat tw-bg-center tw-bg-contain tw-absolute tw-top-3 tw-z-10;
+    content: "";
+    background-image: url("data:image/svg+xml,%3Csvg width='15' height='5' viewBox='0 0 15 5' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M14.0835 1.75H0.583496V0.25H14.0835V1.75ZM14.0835 3.25H0.583496V4.75H14.0835V3.25Z' fill='black' fill-opacity='0.4'/%3E%3C/svg%3E");
+}
+.check-report-1 .q-hoverable:hover > .q-focus-helper {
+    @apply tw-opacity-0 !important;
+}
 
+/* checkbox */
+.check-report-2 .q-field__control-container {
+    @apply tw-rounded-3xl tw-border;
+    border-color: rgba(0,0,0,0.24);
+}
+.check-report-2 .q-hoverable:hover > .q-focus-helper {
+    @apply tw-rounded-3xl;
+}
+.check-report-2 .q-item {
+    @apply tw-pr-5 !important;
+}
+.check-report-2 .q-item__label { 
+    @apply tw-font-medium tw-text-gray-400 tw-text-sm;
+}
+.check-report-2 .q-field--focused {
+    @apply tw-rounded-3xl;
+}
+.check-report-2 .q-checkbox[aria-checked="true"]:before  {
+    @apply tw-w-full tw-h-full tw-absolute tw-rounded-3xl tw-z-0;
+    content: '';
+    background: var(--q-color-primary);
+}
+.check-report-2 .q-item__label {
+    @apply tw-relative;
+    z-index: 1;
+}
+.check-report-2 .q-checkbox[aria-checked="false"] .q-checkbox__bg {
+    @apply tw-border-0;
+}
+.check-report-2 .q-checkbox[aria-checked="false"] .q-checkbox__inner,
+.check-report-2 .q-checkbox[aria-checked="mixed"] .q-checkbox__inner {
+    @apply tw-w-0; 
+    min-width: 20px;
+}
+.check-report-2 .q-checkbox[aria-checked="true"] .q-checkbox__bg  {
+    @apply tw-border-0 tw-bg-transparent;
+}
+.check-report-2 .q-checkbox[aria-checked="mixed"] .q-checkbox__inner--indet .q-checkbox__bg {
+   @apply tw-hidden;
+}
 </style>
--- _components/reports/steppers.vue
+++ _components/reports/steppers.vue
@@ -1,6 +1,13 @@
 
 <template>
-    <div class="q-pa-md stepper-report">
+    <div class="stepper-report">
+      <div class="tw-flex tw-flex-col md:tw-flex-row tw-items-end tw-justify-between">
+        <h2 class="text-primary tw-text-2xl tw-font-semibold tw-mb-4">Create a Report</h2>
+        <div class="text-right">
+          <q-btn rounded outline color="secondary" label="Discard" class="q-mr-sm tw-mb-4" no-caps />    
+          <q-btn rounded color="secondary" label="Save" class="tw-mb-4" no-caps/>
+        </div>
+      </div>
         <q-stepper v-model="step" ref="stepper" color="primary" alternative-labels active-color="primary"  animated class="tw-bg-white">
             <q-step
                 v-for="section in sections"
@@ -15,8 +22,8 @@
 
             <template v-slot:navigation>
                 <q-stepper-navigation class="text-right tw-border-t">
-                    <q-btn outline color="primary" v-if="step > 1"   @click="$refs.stepper.previous()" label="Back" class="q-mr-sm tw-mt-6" />    
-                    <q-btn color="primary" @click="$refs.stepper.next()" :label="step === 4 ? 'Finish' : 'Continue'" class="tw-mt-6"/>
+                    <q-btn rounded no-caps outline icon="fas fa-arrow-left" label="Back" color="primary" v-if="step > 1"  @click="$refs.stepper.previous()" class="q-mr-sm tw-mt-6 text-sm btn-small rounded-sm" />   
+                    <q-btn rounded no-caps color="primary" icon-right="fas fa-arrow-right" @click="$refs.stepper.next()" :label="step === 5 ? 'Finish' : 'Next'" class="tw-mt-6 text-sm btn-small"/>
                 </q-stepper-navigation>
             </template>
         </q-stepper>
@@ -45,94 +52,81 @@ export default {
   @apply tw-block tw-w-full tw-h-px tw-bg-gray-200 tw-top-2/4 tw-absolute tw-z-10;
 }
 .stepper-report .q-stepper {
-  @apply tw-rounded-xl;
+  @apply tw-rounded-xl tw-border tw-shadow-none;
+  border-color: #D3DAEC;
 }
 .stepper-report  .q-stepper__header {
   @apply tw-border-b-0;
 }
-/* cuando se actualice colocar w y h 50px */
+
 .stepper-report .q-stepper__tab  .q-stepper__dot {
-  @apply tw-w-12 tw-h-12 tw-font-bold tw-text-base tw-border-2;
+  @apply tw-w-10 tw-h-10 tw-font-bold tw-text-base tw-border-0;
 }
 .stepper-report .q-stepper__tab:not(.q-stepper__tab--active) .q-stepper__dot {
-  @apply tw-bg-white tw-border-gray-300;
+  @apply tw-border-2;
+  background-color: #F1F4FA;
+  border-color: #D3DAEC;
 }
 .stepper-report .q-stepper__tab:not(.q-stepper__tab--active) .q-stepper__dot span {
    @apply tw-text-gray-400;
 }
 .stepper-report .q-stepper__tab--active .q-stepper__dot {
-  @apply tw-border-current;
+  @apply tw-border-current tw-border-2;
+  border-color: var(--q-color-primary);
 }
 .stepper-report .q-stepper__tab--active .q-stepper__dot span {
   @apply tw-text-white;
 }
 .stepper-report .q-stepper .q-stepper__dot:before {
-  @apply tw-mr-px;
+  @apply tw-mr-8;
 }
 .stepper-report .q-stepper .q-stepper__dot:after {
-  @apply tw-ml-0.5;
+  @apply tw-ml-8;
 }
 .stepper-report .q-stepper .q-stepper__line:after, 
 .stepper-report .q-stepper .q-stepper__line:before {
   @apply tw-h-0.5;
 }
 .stepper-report .q-stepper__title {
-  @apply tw-text-base tw-font-black;
+  @apply tw-text-base tw-font-normal tw-text-black;
 }
-.input-report  .q-field .q-field__label, 
-.radio-report  .q-field .q-field__label {
-  @apply tw-text-black tw-font-medium tw-text-sm tw-top-0;
+.stepper-report  .q-stepper__nav {
+  background-color: #F1F4FA;
+}
+
+.input-report  .q-field .q-field__label {
+  @apply tw-font-medium;
+  color: var(--q-color-primary);
+}
+.radio-report .q-field .q-field__label {
+  @apply tw-font-medium tw-top-0 tw-text-black;
   height: 30px;
   transform: none !important;
 }
-.radio-report .q-field .q-radio__label {
+.radio-report .q-radio__label {
   @apply tw-text-black;
 }
-.input-report  .q-field .q-field__native {
-  @apply tw-font-normal tw-text-sm tw-text-gray-600 tw--mb-1;
-  padding: 8px 20px !important;
+.input-report .q-field__bottom .q-field__messages {
+  @apply tw-text-xs;
+  color: #818181;
 }
-.input-report  .q-field.q-textarea .q-field__native {
-  padding: 10px 15px !important;
+.input-report .q-field__bottom .q-field__counter {
+  @apply tw-text-xs;
 }
-.input-report  .q-field .q-field__control {
-  min-height: 35px;
+.input-report-nolabel  .q-field .q-field__label {
+  @apply tw-font-medium lg:tw-hidden;
+  color: var(--q-color-primary);
 }
-.input-report .q-field:not(.q-textarea) .q-field__control {
-  height: 60px;
+.input-report  .q-field .q-field__native input {
+  @apply lg:tw--mt-3;
 }
-.input-report .q-field .q-field__control:before {
-  @apply tw-border tw-border-gray-300;
-  border-radius: 5px !important;
-  top: 25px;
+.input-report .q-field__native .q-field__input[aria-expanded="false"] ~ .q-field__label {
+  color: red;
 }
-.input-report .q-field .q-field__control:after  {
-  @apply tw-mt-6;
-  border-radius: 5px !important;
+.input-report-nolabel .q-field--labeled .q-field__control-container {
+  @apply lg:tw-pt-px !important;
 }
-.input-report .q-field:not(.q-textarea)  .q-field__control:after {
-  height: 35px;
-}
-.input-report .q-field .q-field__control-container {
-  @apply tw-pb-0;
-  padding-top: 24px !important;
-}
-.input-report .q-field .q-icon {
-  @apply tw-absolute tw-bottom-px tw-text-base;
-  height: 33px;
-  width: 33px;
-  padding-left: 3px;
-}
-.input-report .q-field .q-field__before, .input-report .q-field .q-field__prepend {
-   @apply tw-pr-4;
-}
-.input-report .q-field .q-field__append .q-icon {
-  @apply tw-right-0;
-}
-.input-report .q-field .q-field__prepend +  .q-field__control-container .q-field__label {
-  @apply tw--left-3.5;
-}
-.input-report .q-field--outlined .q-field__control {
-  padding: 0 !important;
+.input-report .q-select__dropdown-icon, .input-report-nolabel .q-select__dropdown-icon {
+  color: var(--q-color-primary);
 }
 </style>
