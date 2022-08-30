964c28c76d407445ca3510cbd1680ccd2fdf4e44
Wilmer Cristancho <wilmerc@imaginacolombia.com>
2022-08-30 11:13:09 -0500

    formulario descripcion realizado

--- _store/sections/descriptionStore.js
+++ _store/sections/descriptionStore.js
@@ -1,16 +1,22 @@
 import { reactive } from '@vue/composition-api';
 import baseService from '@imagina/qcrud/_services/baseService.js';
 import Vue from "vue";
-
+const modelEmailNotification = {
+    email: null,
+    status: false,
+}
 const descriptionModel = {
     reportType: null,
     folder: null,
     reportTitle: null,
     description: null,
-    shortDescription: null,
-    emailNotification: null,
-    isEmail: false,
-    typeReport: null,
+    emailNotification: [
+        {
+            email: null,
+            status: false,
+        }
+    ],
+    attachReport: 'pdf'
 }
 
 const state = reactive({
@@ -21,7 +27,15 @@ export default function descriptionStore() {
     function getDescriptionForm() {
         return state.form;
     }
+    function addEmailNotification() {
+        state.form.emailNotification.push({...modelEmailNotification});
+    }
+    function deleteEmailNotification(index) {
+        state.form.emailNotification.splice(index, 1)
+    }
     return {
         getDescriptionForm,
+        addEmailNotification,
+        deleteEmailNotification,
     }
 }
