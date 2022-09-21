b3110193e0cf2149d7e714d9ff289617d75df5b7
Wilmer Cristancho <wilmerc@imaginacolombia.com>
2022-09-21 15:36:00 -0500

    icon

--- _config/adminPages.js
+++ _config/adminPages.js
@@ -9,7 +9,7 @@ export default {
         page: () => import('@imagina/qcrud/_pages/admin/crudPage'),
         layout: () => import('@imagina/qsite/_layouts/master.vue'),
         title: 'ireports.cms.sidebar.reportsInfo',
-        icon: 'fas fa-file-alt',
+        icon: 'fal fa-file-alt',
         subHeader: {
           refresh: true,
         }
