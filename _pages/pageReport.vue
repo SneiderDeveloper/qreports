ed32a23d388feba317330398f92c3ba805f559ad
Wilmerace <wilmerc@imaginacolombia.com>
2023-12-22 10:47:09 -0500

    se elimina la cache filtros

--- _pages/pageReport.vue
+++ _pages/pageReport.vue
@@ -14,6 +14,7 @@ import _ from "lodash";
 import { QSpinnerFacebook } from 'quasar'
 import { SORT_ASC, SORT_DESC } from '../_components/reports/sections/Model/constants.js';
 import { ASCENDING } from '../_store/sections/model/constants.js';
+import cache from "@imagina/qsite/_plugins/cache";
 
 export default {
   data() {
@@ -26,7 +27,7 @@ export default {
   },
   created() {
     this.$nextTick(async function () {
-      this.$filter.reset();
+      await cache.set('app.data.filters', {})
       this.getCrudReport();
     });
   },
