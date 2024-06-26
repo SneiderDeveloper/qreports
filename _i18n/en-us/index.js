e435ae9d4d2e052b7cd0302cd7d52e3af326a554
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2024-06-26 17:14:45 -0500

    + added translations

--- _i18n/en-us/index.js
+++ _i18n/en-us/index.js
@@ -1,7 +1,35 @@
-import layout from './layout'
-import sidebar from './sidebar'
-
 export default {
-  layout,
-  sidebar
+  "ireports": {
+    "cms": {
+      "form": {},
+      "label": {
+        "selectAll": "Select all"
+      },
+      "sidebar": {
+        "reportsInfo": "Reports",
+        "reportInfo": "Report",
+        "foldersInfo": "Folders",
+        "reportCreate": "Create report",
+        "reportEdit": "Edit report"
+      },
+      "identifyYourReport": "Identify your report",
+      "selectColumnsFilters": "Select columns and filters",
+      "selectSortColumns": "Select and sort columns",
+      "selectColumns": "Select the Columns",
+      "dragColumns": "Drag and reorder the columns",
+      "moveColumn": "Move column",
+      "selectFirst": "Select the filters",
+      "selectFirstSummary": "Choose the firts ({total})* filters you need for this report to use quickly.",
+      "detailFilterOptions": "Detail the filter options",
+      "selectInfoYouWantToShowForEachFilter": "Select the info you want to show for each filter",
+      "chooseColumnSorting": "Choose column sorting",
+      "adjustFollowingParameters": "Do you want to schedule it? Adjust the following parameters",
+      "sort": "Sort",
+      "addAnotherEmail": "Add another email",
+      "message": {
+        "selectColumn": "Please select at least one column to continue and a filter",
+        "waitMoment": "Wait a moment we are loading your report"
+      }
+    }
+  }
 }
